b067caef9fb7323ced305f1003edf5d426b5f7b6
bys96 <jkpp0611@gmail.com>
2026-07-22 19:11:49 +0900

    Initial commit

---
+++ giftmarket-api/src/main/java/com/giftmarket/GiftmarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.giftmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftmarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftmarketApiApplication.class, args);
+	}
+
+}
---
+++ giftmarket-api/src/test/java/com/giftmarket/GiftmarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftmarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
