82999c5f671cab08c1ac0827379e12cb01355b01
bys96 <jkpp0611@gmail.com>
2026-09-13 14:57:05 +0900

    chore: startup JFR 진단 및 DEFERRED A/B 설정 제거

--- giftmarket-api/src/main/java/com/giftmarket/GiftmarketApiApplication.java
+++ giftmarket-api/src/main/java/com/giftmarket/GiftmarketApiApplication.java
@@ -2,7 +2,6 @@ package com.giftmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,13 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class GiftmarketApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(
-				GiftmarketApiApplication.class
-		);
-		application.setApplicationStartup(
-				new FlightRecorderApplicationStartup()
-		);
-		application.run(args);
+		SpringApplication.run(GiftmarketApiApplication.class, args);
 	}
 
 }
